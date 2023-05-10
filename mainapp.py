7ac0d3dae8cc911bed6b2469e3b538879cbe599f
zhisen <zzzz4444450@gmail.com>
2023-05-10 21:44:49 +0800

    1. 修改: mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -62,9 +62,10 @@ def Index():
     
     return_dict = {'success': result}
 
-    return render_template('result.html', result = result, return_dict = return_dict)
+    return render_template('index.html', result = result, return_dict = return_dict,RelTagName=['test1','test2','test3'])
 
 
+#抓取FB貼文資訊
 @app.route("/info")
 def Info():
     socialName = ''
@@ -72,34 +73,64 @@ def Info():
     options = Options()
     options.add_argument("--disable-notifications")
     edge = webdriver.Edge('./mesdgedriver', options=options)
-    # edge.get("https://www.facebook.com/")
-
-    # email = edge.find_element("id", "email")
-    # password = edge.find_element("id", "pass")
-
-    # email.send_keys('zzzz4444450@yahoo.com.tw')
-    # password.send_keys('Xxxx5555560')
-    # password.submit()
 
     # time.sleep(3)
-    # edge.get("https://www.facebook.com/hashtag/美食")
-    edge.get(f"{url[socialName]}{tagName}")
-    for x in range(1, 8):
+    edge.get("https://www.facebook.com/hashtag/美食")
+    # edge.get(f"{url[socialName]}{tagName}")
+    for x in range(1, 4):
         edge.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        time.sleep(8)  
+        time.sleep(6) 
     
     soup = BeautifulSoup(edge.page_source, 'lxml')
-    titles = soup.find_all('div', {'class': 'xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a'})
-    post_user = []
-    for title in titles:
-        post = title.find('div', {'dir': 'auto'})
-        if post:
-            post_user.append(post.string)
-            print(post.string) 
+
+    #發文內容
+    texts = soup.find_all('div', {'class': 'xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a'}) 
+    post_text = []
+
+    #發文者名稱
+    names = soup.find_all('a', {'class': 'x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g xt0b8zv xo1l8bm'})
+    post_name = []
+
+    #發文社團
+    clubs = soup.find_all('a', {'class': 'x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f'}) 
+    post_club = []
+
+    #發文時間
+    posts = soup.find_all('div', {'class': 'du4w35lb k4urcfbm l9j0dhe7 sjgh65i0'})
+    time_list = []
+
+    
+
+
+
+    for text in texts:
+        text1 = text.find('div', {'dir': 'auto'})
+        if text1:
+            post_text.append(text1.string)
+            print(text1.string) 
+    
+    for name in names:
+        name1 = name.find('b')
+        if name1:
+            post_name.append(name1.string)
+    
+    for club in clubs:
+        club1 = club.find('span')
+        if club1:
+            post_club.append(club.string)
+
+    for post in posts:
+        time_tag = post.find('span', {'class': 'a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7'})
+        if time_tag:
+            post_time = time_tag.find('span', {'class': 'timestampContent'}).string
+            time_list.append(post_time)
+            print(post_time)
+
+
 
     edge.quit()
 
-    return render_template('info.html', post_user = post_user)
+    return render_template('info.html', post_text = post_text, post_name = post_name, post_club = post_club, time_list = time_list)
 ############################## page ##############################
 
 
