d679385515351e01d8eade5cb01c419e0105f46a
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2023-05-26 09:24:20 +0800

    20230526--shiyan 1. 修改     * 前端-將寫入貼文資訊切成獨立的function 2. 先上傳目前進度

--- static/js/pageFunction/index.js
+++ static/js/pageFunction/index.js
@@ -1,5 +1,5 @@
 // todo create social post
-function CreateSocialPostItem(posterImgSrc,posterName,postImg=[]) {
+function CreateSocialPostItem(posterImgSrc,posterName,postClub,time,content,likeCount,CommentCount,hashtag=[],postImg=[]) {
     // create element
     let postContainer = document.createElement('div');
     let posterImgContainer = document.createElement('div');
@@ -9,6 +9,11 @@ function CreateSocialPostItem(posterImgSrc,posterName,postImg=[]) {
     let postTIme = document.createElement('p');
     let postContent = document.createElement('p');
     let postImgContainer = document.createElement('p');
+    let postHashtagContainer = document.createElement('p');
+    let like = document.createElement('p');
+    let comment = document.createElement('p');
+
+    postContainer.dataset.post = '';
 
     // poster img
     posterImgContainer.className = 'circle-image';
@@ -18,6 +23,18 @@ function CreateSocialPostItem(posterImgSrc,posterName,postImg=[]) {
     // post's poster
     poster.className = 'post-title';
     poster.textContent = '發文者：' + posterName;
+
+    // post club
+    club.className = 'post-title';
+    club.textContent = '社團：' + postClub;
+
+    // post time
+    postTIme.className = 'post-time';
+    postTIme.textContent = '發文時間：' + time;
+
+    // postContent
+    postContent.className = 'post-content';
+    postContent.textContent = '發文內容：' + content;
 
     // post img
     for (let i = 0; i < postImg.length; i++) {
@@ -29,11 +46,41 @@ function CreateSocialPostItem(posterImgSrc,posterName,postImg=[]) {
         postImgContainer.appendChild(postImgElement);
     }
 
+    // hashtag
+    for (let i = 0; i < hashtag.length; i++) {
+        var hashtagElement = document.createElement('a');
+        hashtagElement.textContent = hashtag[i];
+        hashtagElement.className = 'post-hashtag';
+        hashtagElement.href = '127.0.0.1:8080?keyword=' + hashtag[i];
 
+        postHashtagContainer.appendChild(hashtagElement);
+    }
 
+    // like count
+    like.className = 'post-time';
+    like.textContent = '讚：' + likeCount;
+
+    // comment count
+    comment.className = 'post-time';
+    comment.textContent = '留言：' + CommentCount;
+
+    postContainer.appendChild(posterImgContainer);
+    postContainer.appendChild(poster);
+    postContainer.appendChild(club);
+    postContainer.appendChild(postTIme);
+    postContainer.appendChild(postContent);
+    postContainer.appendChild(postImgContainer);
+    postContainer.appendChild(postHashtagContainer);
+    postContainer.appendChild(like);
+    postContainer.appendChild(comment);
+    $("[data-container='socialPost']")[0].appendChild(postContainer);
 }
 
 // Remove all social post
 function RemoveAllSocialPost() {
-    
+    let postCount = $("[data-post]").length;
+
+    for (let i = 0; i < postCount; i++) {
+        $("[data-post]")[0].remove();
+    }
 }
