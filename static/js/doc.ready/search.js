0939dc276c685d7e0c72845b10df4fba4dbb5f1f
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2023-05-31 11:55:59 +0800

    20230531--shiyan

---
+++ static/js/doc.ready/search.js
@@ -0,0 +1,141 @@
+$(function(){
+    ControlMenu();
+    
+    // ! create knowledge map example
+    CreateKnowledgeMap([{ key: 1,category:"icon", text: "xxx",type:"tag" },{ key: 2, text: "xxx xxx" }],[{ from: 1, to: 2}]);
+    // ajax example
+    // $("[data-social-btn='facebook']").on("click",function(){
+    //     AjaxRequest('/search_FB',{'keyword': $("[data-input='keyword']")[0].value},
+    //     function(data){
+    //         // console.log(data);
+    //         console.log(data['post_item']);
+    //         console.log(data['hashtag_detail']);
+
+    //         // 取得容器元素
+    //         var container = $('[data-container="socialPost"]');
+
+    //         // 迭代 post_item 陣列
+    //         for (var i = 0; i < data['post_item'].length; i++) {
+    //             var item = data['post_item'][i];
+                
+    //             // 建立 post-card 元素
+    //             var postCard = $('<div>').addClass('post-card');
+                
+    //             // 建立 circle-image 元素
+    //             var circleImage = $('<div>').addClass('circle-image');
+                
+    //             // 建立圖片元素
+    //             var image = $('<img>').attr('src', item['post_image']).attr('alt', '圖片');
+                
+    //             // 將圖片元素加入 circle-image 元素
+    //             circleImage.append(image);
+                
+    //             // 建立發文者名稱元素
+    //             var postTitle1 = $('<p>').addClass('post-title').text('發文者: ' + item['post_name']);
+                
+    //             // 建立社團元素
+    //             var postTitle2 = $('<p>').addClass('post-title').text('社團: ' + item['post_club']);
+                
+    //             // 建立發文時間元素
+    //             var postTime = $('<p>').addClass('post-time').text('發文時間: ' + item['post_time']);
+                
+    //             // 建立發文內容元素
+    //             var postContent = $('<p>').addClass('post-content').text('發文內容: ' + item['post_text']);
+
+    //             // 建立發文圖片元素
+    //             var postPicture = $('<p>').addClass('post-time');
+                
+    //             // 迭代發文圖片陣列
+    //             for (var j = 0; j < item['post_picture'].length; j++) {
+    //                 var picture = $('<img>').attr('src', item['post_picture'][j]).addClass('content_pic').attr('alt', '圖片');
+                    
+    //                 // 將發文圖片元素加入發文內容元素
+    //                 postPicture.append(picture);
+    //             }
+                
+    //             // 加入發文影片內容
+    //             postContent.text(postContent.text() + item['post_video']);
+
+    //             var postHashtags = null
+
+    //             if (item['post_hashtag'].length > 0) {
+    //                 // 建立發文 Hashtag 元素
+    //                 postHashtags = $('<p>').addClass('post-hashtags').text('發文hashtag內容: ');
+                    
+    //                 // 迭代發文 Hashtag 陣列
+    //                 for (var k = 0; k < item['post_hashtag'].length; k++) {
+    //                     var hashtag = $('<span>').addClass('post-hashtag').text(item['post_hashtag'][k]);
+                        
+    //                     // 將發文 Hashtag 元素加入發文 Hashtag 內容元素
+    //                     postHashtags.append(hashtag);
+    //                 }
+    //             }
+
+    //             // 建立貼文讚數元素
+    //             var postLikes = item["post_likes"];
+    //             var postLikesElement = $('<p>').addClass('post-time').text('貼文讚數: ' + postLikes);
+
+    //             // 建立貼文留言數元素
+    //             var postComments = item["post_comments"];
+    //             var postCommentsElement = $('<p>').addClass('post-time').text('貼文留言數: ' + postComments);
+
+                
+    //             // 將子元素加入 post-card 元素
+    //             postCard.append(circleImage);
+    //             postCard.append(postTitle1);
+    //             postCard.append(postTitle2);
+    //             postCard.append(postTime);
+    //             postCard.append(postContent);
+    //             postCard.append(postPicture);
+                
+    //             // 如果沒有標hashtag，就不會顯示hashtag的div區塊元素
+    //             if (postHashtags != null) {
+    //                 postCard.append(postHashtags);
+    //             }
+
+    //             postCard.append(postLikesElement);
+    //             postCard.append(postCommentsElement);
+
+    //             // 將 post-card 元素加入容器元素
+    //             container.append(postCard);
+
+
+    //         }
+    //     });
+    // })
+
+    $("[data-close-block-ui]").on("click", function(){
+        $("[data-block-ui='" + this.dataset.closeBlockUi + "']")[0].style.display = "none";
+    });
+    
+    // click social button
+    $("[data-social-btn='facebook']").on("click", function(){
+        if($("[data-input='keyword']")[0].value == ''){
+            alert('請先在搜尋欄輸入想查詢的Hashtag！');
+            return;
+        } else {
+            AjaxRequest('/search_FB',{'keyword': $("[data-input='keyword']")[0].value},
+            function(data){
+                // remove original data
+                RemoveAllSocialPost();
+    
+                for (var i = 0; i < data['post_item'].length; i++) {
+                    // create social post
+                    CreateSocialPostItem(data['post_item'][i]['post_image'],data['post_item'][i]['post_name'],data['post_item'][i]['post_club'],data['post_item'][i]['post_time'],data['post_item'][i]['post_text'],data['post_item'][i]['post_likes'],data['post_item'][i]['post_comments'],data['post_item'][i]['post_hashtag'],data['post_item'][i]['post_picture']);
+                }
+            });
+        }
+    });
+
+    $("[data-social-btn='instagram']").on("click", function(){
+
+    });
+
+    $("[data-social-btn='twitter']").on("click", function(){
+
+    });
+
+    $("[data-social-btn='tictagtoe']").on("click", function(){
+
+    });
+})
---
+++ static/js/pageFunction/search.js
@@ -0,0 +1,204 @@
+// todo create social post
+function CreateSocialPostItem(posterImgSrc,posterName,postClub,time,content,likeCount,CommentCount,hashtag=[],postImg=[]) {
+    // create element
+    let postContainer = document.createElement('div');
+    let posterImgContainer = document.createElement('div');
+    let posterImg = document.createElement('img');
+    let poster = document.createElement('p');
+    let club = document.createElement('p');
+    let postTIme = document.createElement('p');
+    let postContent = document.createElement('p');
+    let postImgContainer = document.createElement('p');
+    let postHashtagContainer = document.createElement('p');
+    let like = document.createElement('p');
+    let comment = document.createElement('p');
+
+    postContainer.dataset.post = '';
+
+    // poster img
+    posterImgContainer.className = 'circle-image';
+    posterImg.src = posterImgSrc;
+    posterImgContainer.appendChild(posterImg);
+
+    // post's poster
+    poster.className = 'post-title';
+    poster.textContent = '發文者：' + posterName;
+
+    // post club
+    club.className = 'post-title';
+    club.textContent = '社團：' + postClub;
+
+    // post time
+    postTIme.className = 'post-time';
+    postTIme.textContent = '發文時間：' + time;
+
+    // postContent
+    postContent.className = 'post-content';
+    postContent.textContent = '發文內容：' + content;
+
+    // post img
+    for (let i = 0; i < postImg.length; i++) {
+        var postImgElement = document.createElement('img');
+
+        postImgElement.src = postImg[i];
+        postImgElement.className = '';
+        postImgElement.alt = '圖片';
+        postImgContainer.appendChild(postImgElement);
+    }
+
+    // hashtag
+    for (let i = 0; i < hashtag.length; i++) {
+        var hashtagElement = document.createElement('a');
+        hashtagElement.textContent = hashtag[i];
+        hashtagElement.className = 'post-hashtag';
+        hashtagElement.href = '127.0.0.1:8080?keyword=' + hashtag[i];
+
+        postHashtagContainer.appendChild(hashtagElement);
+    }
+
+    // like count
+    like.className = 'post-time';
+    like.textContent = '讚：' + likeCount;
+
+    // comment count
+    comment.className = 'post-time';
+    comment.textContent = '留言：' + CommentCount;
+
+    postContainer.appendChild(posterImgContainer);
+    postContainer.appendChild(poster);
+    postContainer.appendChild(club);
+    postContainer.appendChild(postTIme);
+    postContainer.appendChild(postContent);
+    postContainer.appendChild(postImgContainer);
+    postContainer.appendChild(postHashtagContainer);
+    postContainer.appendChild(like);
+    postContainer.appendChild(comment);
+    $("[data-container='socialPost']")[0].appendChild(postContainer);
+}
+
+// Remove all social post
+function RemoveAllSocialPost() {
+    let postCount = $("[data-post]").length;
+
+    for (let i = 0; i < postCount; i++) {
+        $("[data-post]")[0].remove();
+    }
+}
+
+// create knowledge map
+/*
+    * example 
+    * nodeData =[{ key: 1,category:"icon", text: "xxx",type:"tag" },{ key: 2, text: "xxx xxx" }...]
+    * linkData = [{ from: nodeData key, to: nodeData key,},{ from: nodeData key, to: nodeData key}...]
+*/
+function CreateKnowledgeMap(nodeData,linkData) {
+    var $ = go.GraphObject.make;  // for conciseness in defining templates
+    myDiagram =
+    $(go.Diagram, "knowledgeMap",  // must name or refer to the DIV HTML element
+        {
+        initialAutoScale: go.Diagram.Uniform,  // an initial automatic zoom-to-fit
+        contentAlignment: go.Spot.Center,  // align document to the center of the viewport
+        layout:
+        $(go.ForceDirectedLayout,  // automatically spread nodes apart
+            { maxIterations: 200, defaultSpringLength: 30, defaultElectricalCharge: 100 })
+        });
+    // define each Node's appearance
+        myDiagram.nodeTemplate =
+        $(go.Node, "Auto",  // the whole node panel
+            { locationSpot: go.Spot.Center },
+            // define the node's outer shape, which will surround the TextBlock
+        $(go.Shape, "Rectangle",
+            { fill: $(go.Brush, "Linear", { 0: "rgb(254, 201, 0)", 1: "rgb(254, 162, 0)" }), stroke: "black" }),
+        $(go.TextBlock,
+            { font: "bold 10pt helvetica, bold arial, sans-serif", margin: 4 },
+    new go.Binding("text", "text"))
+   );
+ // replace the default Link template in the linkTemplateMap
+    myDiagram.nodeTemplateMap.add("icon",
+        $(go.Node, "Auto",
+            $(go.Panel, "Vertical",
+                // $(go.Shape, "Circle",{ fill: "lightgreen", stroke: "black", strokeWidth: 1, width: 40, height: 40 }),
+                $(go.Picture,{ source: "../static/img/icon.png", width: 30, height: 30 })
+            )
+            //,
+            // $(go.TextBlock,{ margin: 8, font: "12px sans-serif" },
+            //     new go.Binding("text", "text")
+            // )
+        )
+    );
+    myDiagram.linkTemplate =
+    $(go.Link,  // the whole link panel
+    $(go.Shape, { stroke: "black" }),// the link shape
+    // $(go.Shape,{ toArrow: "standard", stroke: null }), // the arrowhead
+    $(go.Panel, "Auto",
+    $(go.TextBlock,  // the label text
+    {
+        textAlign: "center",
+        font: "10pt helvetica, arial, sans-serif",
+        stroke: "#555555",
+        margin: 4
+    },
+    //  new go.Binding("text", "text"))
+     new go.Binding())
+     )
+   );
+ // create the model for the concept map
+//  var nodeDataArray = [
+//   { key: 1,category:"icon", text: "Concept Maps",type:"tag" },
+//   { key: 2, text: "Organized Knowledge" },
+//   { key: 3, text: "Context Dependent" },
+//   { key: 4, text: "Concepts" },
+//   { key: 5, text: "Propositions" },
+//   { key: 6, text: "Associated Feelings or Affect" },
+//   { key: 7, text: "Perceived Regularities" },
+//   { key: 8, text: "Labeled" },
+//   { key: 9, text: "Hierarchically Structured" },
+//   { key: 10, text: "Effective Teaching" },
+//   { key: 11, text: "Crosslinks" },
+//   { key: 12, text: "Effective Learning" },
+//   { key: 13, text: "Events (Happenings)" },
+//   { key: 14, text: "Objects (Things)" },
+//   { key: 15, text: "Symbols" },
+//   { key: 16, text: "Words" },
+//   { key: 17, text: "Creativity" },
+//   { key: 18, text: "Interrelationships" },
+//   { key: 19, text: "Infants" },
+//   { key: 20, text: "Different Map Segments" }
+//  ];
+    var nodeDataArray = nodeData;
+//  var linkDataArray = [
+//   { from: 1, to: 2, text: "represent" },
+//   { from: 2, to: 3, text: "is" },
+//   { from: 2, to: 4, text: "is" },
+//   { from: 2, to: 5, text: "is" },
+//   { from: 2, to: 6, text: "includes" },
+//   { from: 2, to: 10, text: "necessary\nfor" },
+//   { from: 2, to: 12, text: "necessary\nfor" },
+//   { from: 4, to: 5, text: "combine\nto form" },
+//   { from: 4, to: 6, text: "include" },
+//   { from: 4, to: 7, text: "are" },
+//   { from: 4, to: 8, text: "are" },
+//   { from: 4, to: 9, text: "are" },
+//   { from: 5, to: 9, text: "are" },
+//   { from: 5, to: 11, text: "may be" },
+//   { from: 7, to: 13, text: "in" },
+//   { from: 7, to: 14, text: "in" },
+//   { from: 7, to: 19, text: "begin\nwith" },
+//   { from: 8, to: 15, text: "with" },
+//   { from: 8, to: 16, text: "with" },
+//   { from: 9, to: 17, text: "aids" },
+//   { from: 11, to: 18, text: "show" },
+//   { from: 12, to: 19, text: "begins\nwith" },
+//   { from: 17, to: 18, text: "needed\nto see" },
+//   { from: 18, to: 20, text: "between" }
+//  ];
+    var linkDataArray = linkData;
+    myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+    myDiagram.addDiagramListener("ObjectSingleClicked", function(e) {
+    var part = e.subject.part;
+    if (part instanceof go.Node) {
+        var node = part;
+        alert("Node clicked: " + node.data.type);
+    }
+ });
+}
