82562ef72f3d7c183896883e421806523ea58704
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2023-05-31 10:24:37 +0800

    20230531--shiyan 1. 上傳目前進度

--- static/js/doc.ready/index.js
+++ static/js/doc.ready/index.js
@@ -103,20 +103,28 @@ $(function(){
     //         }
     //     });
     // })
+
+    $("[data-close-block-ui]").on("click", function(){
+        $("[data-block-ui='" + this.dataset.closeBlockUi + "']")[0].style.display = "none";
+    });
     
     // click social button
     $("[data-social-btn='facebook']").on("click", function(){
-        AjaxRequest('/search',{'keyword': $("[data-input='keyword']")[0].value},
-        function(data){
-            // remove original data
-            RemoveAllSocialPost();
-
-            for (var i = 0; i < data['post_item'].length; i++) {
-                // create social post
-                CreateSocialPostItem(data['post_item'][i]['post_image'],data['post_item'][i]['post_name'],data['post_item'][i]['post_club'],data['post_item'][i]['post_time'],data['post_item'][i]['post_text'],data['post_item'][i]['post_likes'],data['post_item'][i]['post_comments'],data['post_item'][i]['post_hashtag'],data['post_item'][i]['post_picture']);
-            }
-        });
-        
+        if($("[data-input='keyword']")[0].value == ''){
+            alert('請先在搜尋欄輸入想查詢的Hashtag！');
+            return;
+        } else {
+            AjaxRequest('/search',{'keyword': $("[data-input='keyword']")[0].value},
+            function(data){
+                // remove original data
+                RemoveAllSocialPost();
+    
+                for (var i = 0; i < data['post_item'].length; i++) {
+                    // create social post
+                    CreateSocialPostItem(data['post_item'][i]['post_image'],data['post_item'][i]['post_name'],data['post_item'][i]['post_club'],data['post_item'][i]['post_time'],data['post_item'][i]['post_text'],data['post_item'][i]['post_likes'],data['post_item'][i]['post_comments'],data['post_item'][i]['post_hashtag'],data['post_item'][i]['post_picture']);
+                }
+            });
+        }
     });
 
     $("[data-social-btn='instagram']").on("click", function(){
